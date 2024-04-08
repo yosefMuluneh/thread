f7d8595264905485b977acfad87f646223706130
Yosef Muluneh <119527329+yosefMuluneh@users.noreply.github.com>
2024-04-08 16:19:27 +0300

    Update page.tsx

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -7,8 +7,12 @@ import { redirect } from 'next/navigation'
 import { fetchThreadById } from '@/lib/actions/thread.actions'
 import Comment from '@/components/forms/Comment'
 
-export const dynamicParams = false
-export const dynamic = 'force-static'
+export const dynamicParams = async () => { 
+    return false
+}
+export const dynamic = async () => {
+    return 'force-static'
+}
 
 
 const page =  async({ params } : { params : { id : string}}) => {
