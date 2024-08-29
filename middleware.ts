f7f4830563f2ce824914d9a05599fc6401f8d456
yosefMuluneh <yosefmulunehof2020@gmail.com>
2024-08-29 12:30:03 +0300

    update middleware for clerk

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import {  authMiddleware } from '@clerk/nextjs/server'
 
-export default authMiddleware()
+export default authMiddleware({
+  publicRoutes: ['/','/api/webhook/clerk'],
+  ignoredRoutes: ['/api/webhook/clerk']
+})
 
 export const config = {
   matcher: [
