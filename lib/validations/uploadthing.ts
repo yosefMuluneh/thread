d26249e031118ace4e6c100899af7e0054fc3863
yosefMuluneh <yosefmulunehof2020@hmail.com>
2026-08-25 23:09:23 +0300

    fix

--- lib/validations/uploadthing.ts
+++ lib/validations/uploadthing.ts
@@ -1,7 +1,7 @@
 // Resource: https://docs.uploadthing.com/api-reference/react#generatereacthelpers
 // Copy paste (be careful with imports)
 
-import { generateReactHelpers } from "@uploadthing/react/hooks";
+import { generateReactHelpers } from "@uploadthing/react";
 
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
